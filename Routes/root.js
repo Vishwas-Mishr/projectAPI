ba051772b11dddb79c411ad8b2e37cc46823f823
Vishwas-Mishr <74827986+Vishwas-Mishr@users.noreply.github.com>
2023-07-15 11:37:39 +0530

    Update root.js

--- Routes/root.js
+++ Routes/root.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send("<h1>Read the <strong>README</strong> file.</h1>");
+    res.send("<h1>Read the <a href=\"https://github.com/Vishwas-Mishr/projectAPI/blob/main/README.md\" target=__blank><strong>README</strong> file.</a></h1>");
 });
 
 module.exports = router;
