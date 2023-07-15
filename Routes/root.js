05d6b1a2b7f84607505a5da1b74c3d706fed0226
Vishwas-Mishr <74827986+Vishwas-Mishr@users.noreply.github.com>
2023-07-15 11:34:32 +0530

    Update root.js

--- Routes/root.js
+++ Routes/root.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    res.send(<h1>Read the <strong>README</strong> file.</h1>);
+    res.send("<h1>Read the <strong>README</strong> file.</h1>");
 });
 
 module.exports = router;
